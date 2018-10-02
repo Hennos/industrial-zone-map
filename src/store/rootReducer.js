73ea865d4461e35249e2a559bda8cfce5a28dc10
Evgeny Karasev <gnomskg@gmail.com>
2018-10-02 15:00:19 +0300

    connect added reducer

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -2,10 +2,14 @@ import { combineReducers } from 'redux';
 
 import search from './search/reducer';
 import legend from './legend/reducer';
+import entityInformation from './entityInformation/reducer';
+import objectDetails from './objectDetails/reducer';
 
 const rootReducer = combineReducers({
   search,
   legend,
+  entityInformation,
+  objectDetails,
 });
 
 export default rootReducer;
