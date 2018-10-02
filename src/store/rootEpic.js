382cc0efdf7d36e9572a68b95987aa0cca041fb0
Evgeny Karasev <gnomskg@gmail.com>
2018-10-02 14:59:56 +0300

    connect added epics

--- src/store/rootEpic.js
+++ src/store/rootEpic.js
@@ -2,10 +2,14 @@ import { combineEpics } from 'redux-observable';
 
 import legend from './legend/epic';
 import search from './search/epic';
+import entityInformation from './entityInformation/epic';
+import objectDetails from './objectDetails/epic';
 
 const rootEpic = combineEpics(
   legend,
   search,
+  entityInformation,
+  objectDetails,
 );
 
 export default rootEpic;
