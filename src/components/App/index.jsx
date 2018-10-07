d01f1b7a111c5bc67f18a785c477c1da1818f8aa
Evgeny Karasev <gnomskg@gmail.com>
2018-10-07 16:28:38 +0300

    Add loading status to App component

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,17 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import './index.css';
+
+import { loadUserStatus } from '../../store/status/actions';
 
 import Header from '../Header';
 import Navigation from '../Navigation';
 import Content from '../Content';
 
-const App = () => (
-  <div className="map-application">
-    <Header stylization="map-application-header map-application-fragment" />
-    <Navigation stylization="map-application-nav map-application-fragment" />
-    <Content stylization="map-application-content map-application-fragment" />
-  </div>
-);
+class App extends React.Component {
+  componentDidMount() {
+    this.props.onLoadUserStatus();
+  }
 
-export default App;
+  render() {
+    return (
+      <div className="map-application">
+        <Header stylization="map-application-header map-application-fragment" />
+        <Navigation stylization="map-application-nav map-application-fragment" />
+        <Content stylization="map-application-content map-application-fragment" />
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  onLoadUserStatus: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = dispatch => ({
+  onLoadUserStatus: () => dispatch(loadUserStatus()),
+});
+
+export default connect(undefined, mapDispatchToProps)(App);
