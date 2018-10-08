02643ecc4ed57f32453f0264d9a97393c8ea1e7d
Evgeny Karasev <gnomskg@gmail.com>
2018-10-08 19:53:06 +0300

    add visability controlling for modal window in Content component

--- src/components/Content/index.jsx
+++ src/components/Content/index.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import './index.css';
 
-import Sidebar from './components/Sidebar';
+import { keys as searchKeys } from '../../store/search/constants';
+import { keys as legendKeys } from '../../store/legend/constants';
 
+import Sidebar from './components/Sidebar';
 import Map from './components/Map';
 import EntityInformation from './components/EntityInformation';
 import MapInformation from './components/MapInformation';
+import SearchFilters from './components/SearchFilters';
+import MapLegend from './components/MapLegend';
 
-const Content = ({ stylization }) => (
+const Content = ({ stylization, filtersVisibility, legendVisability }) => (
   <div className={classNames(stylization, 'content')}>
     <Sidebar stylization="content-sidebar">
       <EntityInformation />
@@ -21,15 +26,28 @@ const Content = ({ stylization }) => (
     <div className="content-footer">
       <MapInformation />
     </div>
+    {filtersVisibility && <SearchFilters
+      stylization="content-search-filters modal-window-theme"
+    />}
+    {legendVisability && <MapLegend
+      stylization="content-map-legend modal-window-theme"
+    />}
   </div>
 );
 
 Content.propTypes = {
   stylization: PropTypes.string,
+  filtersVisibility: PropTypes.bool.isRequired,
+  legendVisability: PropTypes.bool.isRequired,
 };
 
 Content.defaultProps = {
   stylization: '',
 };
 
-export default Content;
+const mapStateTpProps = state => ({
+  filtersVisibility: state.search.get(searchKeys.filtersVisability),
+  legendVisability: state.legend.get(legendKeys.legendVisability),
+});
+
+export default connect(mapStateTpProps)(Content);
