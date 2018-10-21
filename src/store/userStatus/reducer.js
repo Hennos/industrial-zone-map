e0b1cfcaf6444b7ce83ccce141cfe25af988a21a
Evgeny Karasev <gnomskg@gmail.com>
2018-10-21 15:41:56 +0300

    fix parse user status state getting by response

--- src/store/userStatus/reducer.js
+++ src/store/userStatus/reducer.js
@@ -10,16 +10,16 @@ import initialState from './initialState';
 function handleSetUserStatus(prevState, { user }) {
   const {
     is_authorized: authorized,
-    user_groups: userGroups,
   } = user;
+  const userGroups = user.user_groups || {};
 
   const groups = Immutable.Map([
-    [userGroupsEnum.fl, userGroups.fl],
-    [userGroupsEnum.ul, userGroups.ul],
-    [userGroupsEnum.iogv, userGroups.iogv],
-    [userGroupsEnum.admin, userGroups.admin],
-    [userGroupsEnum.operator, userGroups.operator],
-    [userGroupsEnum.approved, userGroups.approved],
+    [userGroupsEnum.fl, userGroups.fl || null],
+    [userGroupsEnum.ul, userGroups.ul || null],
+    [userGroupsEnum.iogv, userGroups.iogv || null],
+    [userGroupsEnum.admin, userGroups.admin || null],
+    [userGroupsEnum.operator, userGroups.operator || null],
+    [userGroupsEnum.approved, userGroups.approved || null],
   ]);
 
   return prevState
