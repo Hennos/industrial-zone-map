4310fb09e35c839668e88482bbcf2368479789b1
Evgeny Karasev <gnomskg@gmail.com>
2018-10-22 16:06:25 +0300

    add editing property to InputEditableProperty component

--- src/components/Content/components/InputEditableProperty/index.jsx
+++ src/components/Content/components/InputEditableProperty/index.jsx
@@ -9,22 +9,17 @@ const InputEditableProperty = ({
   value,
   data,
   onChange,
-}) => {
-  const Input = () => (
+}) => (
+  <div className={classNames(stylization, 'input-editable-property')}>
+    {data.title}:
     <input
       className="input-editable-property-input"
       type="text"
       value={value}
-      onChange={onChange}
+      onChange={event => onChange(event.target.value)}
     />
-  );
-
-  return (
-    <div className={classNames(stylization, 'input-editable-property')}>
-      {data.title}: <Input />
-    </div>
-  );
-};
+  </div>
+);
 
 const shapeData = {
   title: PropTypes.string.isRequired,
@@ -33,7 +28,7 @@ const shapeData = {
 InputEditableProperty.propTypes = {
   stylization: PropTypes.string,
   data: PropTypes.shape(shapeData).isRequired,
-  value: PropTypes.string,
+  value: PropTypes.any,
   onChange: PropTypes.func,
 };
 
