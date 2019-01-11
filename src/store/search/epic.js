c40c9c5997f929e93f167399586e218f6efc74bc
Evgeny Karasev <gnomskg@gmail.com>
2019-01-11 18:08:58 +0300

    fix code style on module search

--- src/store/search/epic.js
+++ src/store/search/epic.js
@@ -5,16 +5,13 @@ import { combineEpics, ofType } from 'redux-observable';
 
 import { events as loaderEvents } from '../loader/constants';
 import { keys as searchKeys, events as searchEvents } from './constants';
-import {
-  setFiltersData,
-  getFoundObjects,
-  errorGetFoundObjects,
-} from './actions';
+import { setFiltersData, getFoundObjects, errorGetFoundObjects } from './actions';
 
-const setFiltersDataEpic = action$ => action$.pipe(
-  ofType(loaderEvents.successLoadFilters),
-  map(({ data }) => setFiltersData(data.records)),
-);
+const setFiltersDataEpic = action$ =>
+  action$.pipe(
+    ofType(loaderEvents.successLoadFilters),
+    map(({ data }) => setFiltersData(data.records))
+  );
 
 const enumerateSearchConfigurator = (search, filters) => {
   return JSON.stringify({
@@ -27,30 +24,37 @@ const enumerateSearchConfigurator = (search, filters) => {
       .map(({ property, value, data }) => ({
         property,
         value,
-        operation: data.type === 'range' ? 'is_range' : 'is',
-      })).toArray(),
+        operation: data.type === 'range' ? 'is_range' : 'is'
+      }))
+      .toArray()
   });
 };
 const requestSearchObjectsURI = request =>
   `http://industry.specom-vm.ru/map_interface.php?action=enumerate&data=${request}`;
-const loadFoundObjectsEpic = (action$, state$) => action$.pipe(
-  ofType(searchEvents.requestSearchObjects),
-  mergeMap(({ search }) => ajax.getJSON(requestSearchObjectsURI(enumerateSearchConfigurator(
-    search,
-    state$.value.search.get(searchKeys.filters).map(filter => ({
-      property: filter,
-      value: state$.value.search.get(searchKeys.filtersValue).get(filter),
-      data: state$.value.search.get(searchKeys.filtersData).get(filter),
-    })),
-  ))).pipe(
-    map(response => getFoundObjects(response.objects)),
-    catchError(error => of(errorGetFoundObjects(error))),
-  )),
-);
+const loadFoundObjectsEpic = (action$, state$) =>
+  action$.pipe(
+    ofType(searchEvents.requestSearchObjects),
+    mergeMap(({ search }) =>
+      ajax
+        .getJSON(
+          requestSearchObjectsURI(
+            enumerateSearchConfigurator(
+              search,
+              state$.value.search.get(searchKeys.filters).map(filter => ({
+                property: filter,
+                value: state$.value.search.get(searchKeys.filtersValue).get(filter),
+                data: state$.value.search.get(searchKeys.filtersData).get(filter)
+              }))
+            )
+          )
+        )
+        .pipe(
+          map(response => getFoundObjects(response.objects)),
+          catchError(error => of(errorGetFoundObjects(error)))
+        )
+    )
+  );
 
-const epic = combineEpics(
-  setFiltersDataEpic,
-  loadFoundObjectsEpic,
-);
+const epic = combineEpics(setFiltersDataEpic, loadFoundObjectsEpic);
 
 export default epic;
